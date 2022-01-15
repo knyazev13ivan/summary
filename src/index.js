38923eb86ac45aad8c7e353e3a79e48b12f3a381
Ivan Knyazev <knyazev13ivan@gmail.com>
2022-01-16 02:11:25 +0300

    Add mouse and UI events

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 // import './style/style.css'
 
-import { NavigationComponent } from './components/navigation.component.js'
-import { TermsComponent } from './components/terms.component.js'
+import {
+  NavigationComponent
+} from './components/navigation.component.js'
+import {
+  TermsComponent
+} from './components/terms.component.js'
 
 const navigation = new NavigationComponent('.nav-page')
 
@@ -10,10 +14,40 @@ const jsTerms = new TermsComponent('.js-terms')
 const domTerms = new TermsComponent('.dom-terms')
 const reactTerms = new TermsComponent('.react-terms')
 
-navigation.registerTabs([
-  {name: 'css', component: cssTerms},
-  {name: 'js', component: jsTerms},
-  {name: 'dom', component: domTerms},
-  {name: 'react', component: reactTerms}
+navigation.registerTabs([{
+    name: 'css',
+    component: cssTerms
+  },
+  {
+    name: 'js',
+    component: jsTerms
+  },
+  {
+    name: 'dom',
+    component: domTerms
+  },
+  {
+    name: 'react',
+    component: reactTerms
+  }
 ])
 
+function testing() {
+  let elem = document.createElement('div')
+  elem.style.cssText = `position: absolute;
+  top: 10px;
+  left: 10px;
+  z-index: 100;
+  width: 600px;
+  height: 500px;
+  background-color: #222222;
+  color: #cccccc;`
+  document.body.append(elem)
+
+  // =========================
+  
+
+
+}
+
+// testing()
